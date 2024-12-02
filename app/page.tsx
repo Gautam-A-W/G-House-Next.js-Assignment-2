009ffb905714915fd79fb9c885fdcdf0e092cdc4
Gautam-A-W <gautamashokwaghela@gmail.com>
2024-12-02 12:50:58 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Header from "./header/page";
+import Footer from "./footer/page";
+
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <h1>HOME</h1>
+    <Footer/>
+    </>
+  );
+}
